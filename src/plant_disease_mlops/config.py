dde37821fb187979653883813d5cb4e9d501008a
Mohamed Abdeldayem <mabdeldayem052@gmail.com>
2026-09-17 23:39:26 +0300

    update config.py

--- src/plant_disease_mlops/config.py
+++ src/plant_disease_mlops/config.py
@@ -6,6 +6,12 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 
 class Settings(BaseSettings):
     RAW_DATA_DIR_PATH: Path
+    MODEL_KERAS_PATH: Path
+    MODEL_ONNX_PATH: Path
+    WANDB_KEY: str
+    WANDB_PROJECT_NAME: str
+    WANDB_ENTITY: str
+
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env", env_file_encoding="utf-8"
     )

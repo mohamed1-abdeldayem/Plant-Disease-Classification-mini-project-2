c464664ca96cf6169f5afc80a3d0dbb3803c45ae
Mohamed Abdeldayem <mabdeldayem052@gmail.com>
2026-09-17 23:40:12 +0300

    update preprocessing.py

--- src/plant_disease_mlops/preprocessing.py
+++ src/plant_disease_mlops/preprocessing.py
@@ -2,26 +2,24 @@ from pathlib import Path
 
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from .config import settings
 
 
 class PlantDiseasePreprocessor:
     def __init__(
         self,
-        image_size: tuple[int, int] = (224, 224),
-        batch_size: int = 32,
-        validation_split: float = 0.2,
     ) -> None:
-        self.image_size = image_size
-        self.batch_size = batch_size
-        self.validation_split = validation_split
+        self.data_dir: Path = Path(settings.base_dir_path, settings.RAW_DATA_DIR_PATH)
 
     def get_train_generator(
         self,
-        data_dir: Path,
+        image_size: tuple[int, int] = (224, 224),
+        batch_size: int = 32,
+        validation_split: float = 0.2,
     ):
         train_datagen = ImageDataGenerator(
             rescale=1.0 / 255.0,
-            validation_split=self.validation_split,
+            validation_split=validation_split,
             rotation_range=40,
             width_shift_range=0.2,
             height_shift_range=0.2,
@@ -32,9 +30,9 @@ class PlantDiseasePreprocessor:
         )
 
         return train_datagen.flow_from_directory(
-            data_dir,
-            target_size=self.image_size,
-            batch_size=self.batch_size,
+            self.data_dir,
+            target_size=image_size,
+            batch_size=batch_size,
             class_mode="categorical",
             subset="training",
             shuffle=True,
@@ -42,29 +40,30 @@ class PlantDiseasePreprocessor:
 
     def get_validation_generator(
         self,
-        data_dir: Path,
+        image_size: tuple[int, int] = (224, 224),
+        batch_size: int = 32,
+        validation_split: float = 0.2,
     ):
         val_datagen = ImageDataGenerator(
             rescale=1.0 / 255.0,
-            validation_split=self.validation_split,
+            validation_split=validation_split,
         )
 
         return val_datagen.flow_from_directory(
-            data_dir,
-            target_size=self.image_size,
-            batch_size=self.batch_size,
+            self.data_dir,
+            target_size=image_size,
+            batch_size=batch_size,
             class_mode="categorical",
             subset="validation",
             shuffle=False,
         )
 
     def preprocess_image(
-        self,
-        image_path: str | Path,
+        self, image_path: str | Path, image_size: tuple[int, int] = (224, 224)
     ) -> tf.Tensor:
         image = tf.keras.utils.load_img(
             image_path,
-            target_size=self.image_size,
+            target_size=image_size,
         )
 
         image = tf.keras.utils.img_to_array(image)

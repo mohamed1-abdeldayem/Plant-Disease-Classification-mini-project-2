c14ba6ef4052f43725fb577293c62087703d3bbf
Mohamed Abdeldayem <mabdeldayem052@gmail.com>
2026-09-18 15:29:59 +0300

    add inference.py

--- src/plant_disease_mlops/__init__.py
+++ src/plant_disease_mlops/__init__.py
@@ -1,4 +1,5 @@
 from .config import settings as settings
 from .preprocessing import PlantDiseasePreprocessor as Preprocessor
+from .inference import Inference as Inference
 
-__all__ = ["Preprocessor", "settings"]
+__all__ = ["Preprocessor", "settings", "Inference"]

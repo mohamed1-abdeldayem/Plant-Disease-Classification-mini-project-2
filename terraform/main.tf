c31e61e751c0d18cea3df242d4eda5a9466979ac
Mohamed Abdeldayem <mabdeldayem052@gmail.com>
2026-09-19 00:41:54 +0300

    add terraform infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_network" "plant_disease_network" {
+  name = "plant-disease-network"
+}
+
